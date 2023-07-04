c169097650786c34ba0cfca4c589ffe58da7609e
kazurayam <kazuaki.urayama@gmail.com>
2023-07-05 07:52:26 +0900

    update

---
+++ Object Repository/book_app.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>book_app</name>
+   <tag></tag>
+   <elementGuidId>6fc6753f-a4e4-4e19-bde1-d00b101c2188</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@apptitle = 'BOOKS']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>apptitle</name>
+      <type>Main</type>
+      <value>BOOKS</value>
+      <webElementGuid>704f0ba3-5bb8-4ecb-941d-4c76b11eb1da</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/input.rs
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input</name>
+   <tag></tag>
+   <elementGuidId>786ddfc4-10e9-4ea6-88d9-22692ddedee1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'input']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>input</value>
+      <webElementGuid>a492fa1f-7e8e-4c4c-911e-a7d4c6d8cebb</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/book_app</value>
+      <webElementGuid>d119ad26-7de6-4e1d-8377-03557573677b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element_is_shadow_root</name>
+      <type>Main</type>
+      <value>true</value>
+      <webElementGuid>43faabdb-ec28-4d6b-bc02-be7e8af77241</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
